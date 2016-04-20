fd706de0b2dcafaac387af66f32928d42a5c3f64
William <williamsmyth2603@gmail.com>
2016-04-21 00:02:20 +0100

    added main body of text

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this file are two functions which create a special object
+## that stores a matrix and caches its inverse
 
-## Write a short comment describing this function
+## functions sets up a list of function to set/get/update the matrix 
+## and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x<<-y
+    m<<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m<<- inv
+  getinv <- function() m
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Calculates the inverse of the sdtored matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if (!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinv(m)
+  m
 }
